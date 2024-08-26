ba1ce1707ffff75736dc3a4950cfd458f53d2583
Andreas Vogt <andreas.vogt@freiraumweb.de>
2024-08-26 16:49:16 +0200

    BlogPost#show erstellt

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  get "/blog_posts/:id", to: "blog_posts#show"
+
   # Defines the root path route ("/")
   root "blog_posts#index"
 end
